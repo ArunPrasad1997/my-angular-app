2f2960c52930f8db4fed877479a9e3537a73e6b3
ArunPrasad1997 <arunprasad5655@gmail.com>
2026-01-09 16:33:02 +0530

    Initial page setup for home and about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: 'home', pathMatch: 'full' },
+    { path: "home", component: Home },
+    { path: "about", component: About },
+    { path: "**", redirectTo: 'home' },
+
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutes { }
