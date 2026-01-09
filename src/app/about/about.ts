1811ce0eb9155711a0e671ef64e5b9d7c2c43ffc
ArunPrasad1997 <arunprasad5655@gmail.com>
2026-01-09 18:34:21 +0530

    Learning and walk through the TS functionality

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {
+export class About implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  title = 'About Page';
+  description = 'About this application';
+
+  name = '';
+
+  features = [
+    'Angular',
+    'TypeScript',
+    'Routing',
+    'Components'
+  ];
+
+  showDetails = false;
+
+
+  changeTitle() {
+    this.title = 'About page updated';
+  }
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails
+  }
 }
